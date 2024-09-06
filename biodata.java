37f3f026b7b98907d8aa24ae4d80a77021a8babf
LAPTOP-RBLOQQKD\Naswa Nida <naswanida3@gmail.com>
2024-09-06 08:57:07 +0700

    Biodata Naswanida Nafiula

---
+++ biodata.java
@@ -0,0 +1,18 @@
+public class biodata {
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------------------------");
+        System.out.println("-- Biodata -- ");
+        System.out.println("---------------------------------------------------------------");
+        System.out.println("Nama          : Naswanida Nafiula");
+        System.out.println("Tanggal lahir : 29 Juli 2004");
+        System.out.println("NIM           : 244107060063");
+        System.out.println("Kelas         : SIB 1B");
+        System.out.println("No Absen      : 17");
+        System.out.println("Prodi         : Sistem Informasi Bisnis");
+        System.out.println("Alamat        : Grand Mashyur Residence Blok A9");
+        System.out.println("No HP         : 081217775489");
+        System.out.println("Email         : naswanida3@gmail.com");
+        System.out.println("Hobi          : Tidur");
+
+    }
+}
