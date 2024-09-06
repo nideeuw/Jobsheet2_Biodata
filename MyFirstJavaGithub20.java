c00ad26a1b05626918d71d2ad23057ee9e6b359a
raratii <purnamaratih419@gmail.com>
2024-09-06 10:54:12 +0700

    biodata ratih purnama

---
+++ MyFirstJavaGithub20.java
@@ -0,0 +1,19 @@
+public class MyFirstJavaGithub20 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from Github");
+        System.out.println("-------------------------------------------");
+        System.out.println(" -- BIODATA -- ");
+        System.out.println("--------------------------------------------");
+        System.out.println("NAMA          : RATIH PURNAMA DEWI");
+        System.out.println("TANGGAL LAHIR : 19 AGUSTUS 2005");
+        System.out.println("NIM           : 255107060055");
+        System.out.println("KELAS         : SIB 1B ");
+        System.out.println("NO. ABSEN     : 20");
+        System.out.println("PRODI         : SISTEM INFORMASI BISNIS");
+        System.out.println("ALAMAT        : JL. RANDUAGUNG ");
+        System.out.println("NO.HP         : 085655722538");
+        System.out.println("Email         : purnamaratih419@gmail.com");
+        System.out.println("HOBI          : MAIN");
+    }
+}
