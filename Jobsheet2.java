c44c41cfe2ecaea4dbed2da9572f4bd864da9828
fernandaay7 <fernandaayuputri07@gmail.com>
2024-09-06 11:10:45 +0700

    Biodata Fernanda Ayu

---
+++ Jobsheet2.java
@@ -0,0 +1,18 @@
+public class Jobsheet2 {
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------------------------");
+        System.out.println("-- Biodata -- ");
+        System.out.println("---------------------------------------------------------------");
+        System.out.println("Nama          : Fernanda Ayu");
+        System.out.println("Tanggal lahir : 07 Februari 2006");
+        System.out.println("NIM           : 244107060123");
+        System.out.println("Kelas         : SIB 1B");
+        System.out.println("No Absen      : 08");
+        System.out.println("Prodi         : Sistem Informasi Bisnis");
+        System.out.println("Alamat        : Jl.segoromadu 17");
+        System.out.println("No HP         : 085729705919");
+        System.out.println("Email         : fernandaayuputri07@gmail.com");
+        System.out.println("Hobi          : Kuliner");
+
+    }
+}
